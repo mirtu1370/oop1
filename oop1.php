87481785af51bc6fb8a7799775a8b552b879c492
Mílton <mirtu1370@gmail.com>
2019-04-23 22:05:48 +0100

    oopfile

---
+++ oop1.php
@@ -0,0 +1,17 @@
+<?php
+
+ class Autor{
+ public $nome;
+ public $email;
+ public function _construct($nome, $color){
+ 	$this -> Nome = $nome
+ 	$this -> Exemplomail = $email
+ }
+public function echoData(){
+	echo "The email of {$this -> Nome} is {$this -> Exemplomail}";
+}
+ }
+
+
+
+ ?>
